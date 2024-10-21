06517736462c542add2cb703f7dc14c4fba139a8
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-10-21 20:57:56 -0300

    Add files

---
+++ CMakeProject1.cpp
@@ -0,0 +1,18 @@
+﻿// CMakeProject1.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "CMakeProject1.h"
+#include <sstream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello CMake, I am here" << endl;
+	int i = 100;
+	for (size_t i = 0; i <= 100 ; i++)
+	{
+		cout << "Hello CMake, I am here " <<std::to_string(i) << endl;
+	}
+	return 0;
+}
