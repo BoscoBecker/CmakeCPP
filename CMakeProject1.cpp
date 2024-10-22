6176582876fcc705590843e6929f2f3c0e376249
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-10-21 21:40:55 -0300

    Add more samples

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -3,16 +3,28 @@
 
 #include "CMakeProject1.h"
 #include <sstream>
+#include <iostream>
+#include <list>
+
 using namespace std;
 
 
 int main()
 {
+	// Print
 	cout << "Hello CMake, I am here" << endl;
+	
+	// For
 	int i = 100;
 	for (size_t i = 0; i <= 100 ; i++)
 	{
 		cout << "Hello CMake, I am here " <<std::to_string(i) << endl;
+	}
+
+	// Creating List
+	list<string> gqlist { "Jhon","Bosco" };
+	for (auto i : gqlist) {
+		cout << i << ' - ';
 	}
 	return 0;
 }
